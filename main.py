73f539110fca6c91e661986b4025b2c974f4bac5
camilo-camargo <camilocamargo49@gmail.com>
2023-06-08 06:41:55 -0500

    Replace message handler for command handler.

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import os
 from dotenv import load_dotenv
 import discord
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-
-class OreillyBotClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = OreillyBotClient(intents=intents)
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+@bot.command('hello')
+async def hello(ctx):
+    await ctx.send('world')
+
+bot.run(TOKEN)
