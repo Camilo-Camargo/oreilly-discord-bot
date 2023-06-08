3f2eb700938634f5c436711205e1c7293cca7cdd
camilo-camargo <camilocamargo49@gmail.com>
2023-06-08 13:20:37 -0500

    Add oreilly book send file with command /book.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 run:
-	python book.py
+	python main.py
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from account import read_user_account
+from book import safaribook
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,8 +14,11 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 
-@bot.command('hello')
-async def hello(ctx):
-    await ctx.send('world')
+@bot.command('book')
+async def hello(ctx, arg):
+    book_id = arg
+    account = read_user_account()
+    output_path = safaribook(account[0], account[1], book_id)
+    await ctx.send(file=discord.File(output_path))
 
 bot.run(TOKEN)
