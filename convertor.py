f6fc82b34aea17bbcbcd1bfc13543ba9a178d522
Cristhian Morales <kiki8491@gmail.com>
2023-06-09 10:32:49 -0500

    Created methods to convert

--- convertor.py
+++ convertor.py
@@ -1,2 +1,16 @@
-from ebooklib import epub
-from fpdf import FPDF
+import requests
+import os
+import subprocess
+
+def download_and_convert(url):
+    response = requests.get(url)
+
+    file_name = os.path.basename(url)
+
+    with open(file_name, 'wb') as f:
+        f.write(response.content)
+    
+    file_name_pdf = os.path.splitext(file_name)[0] + '.pdf'
+    subprocess.run(['ebook-convert', file_name, file_name_pdf])
+
+    return file_name_pdf
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 from account import read_user_account
 from book import safaribook
+from convertor import download_and_convert
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -20,5 +21,10 @@ async def hello(ctx, arg):
     account = read_user_account()
     output_path = safaribook(account[0], account[1], book_id)
     await ctx.send(file=discord.File(output_path))
+
+@bot.command('convert')
+async def convert(ctx, url: str):
+    name_file_pdf = download_and_convert(url)
+    await ctx.send(f'Archivo {name_file_pdf} creado.', file=discord.File(name_file_pdf))
 
 bot.run(TOKEN)
