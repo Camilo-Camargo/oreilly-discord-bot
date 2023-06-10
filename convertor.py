35a20ec09b07c9b9b7819e85835b733432ec204a
Cristhian Morales <kiki8491@gmail.com>
2023-06-09 21:05:10 -0500

    Convert epub to pdf and pdf to doc

--- convertor.py
+++ convertor.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import subprocess
+from pdf2docx import Converter 
 
 def download_and_convert(url):
     response = requests.get(url)
@@ -14,3 +15,19 @@ def download_and_convert(url):
     subprocess.run(['ebook-convert', file_name, file_name_pdf])
 
     return file_name_pdf
+def convert_pdf_to_word(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        pdf_filename = "file.pdf"
+        with open(pdf_filename, 'wb') as file:
+            file.write(response.content)
+
+        output_filename = os.path.splitext(pdf_filename)[0] + ".docx"
+        cv = Converter(pdf_filename)
+        cv.convert(output_filename, start=0, end=None)
+        cv.close()
+
+        os.remove(pdf_filename)
+        return output_filename
+    else:
+        raise Exception("The PDF file could not be downloaded.")
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import asyncio
 import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from account import read_user_account
 from book import safaribook
-from convertor import download_and_convert
+from convertor import download_and_convert, convert_pdf_to_word
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,16 +16,38 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 
-@bot.command('book')
+"""@bot.command('book')
 async def hello(ctx, arg):
     book_id = arg
     account = read_user_account()
     output_path = safaribook(account[0], account[1], book_id)
     await ctx.send(file=discord.File(output_path))
-
+"""
 @bot.command('convert')
 async def convert(ctx, url: str):
-    name_file_pdf = download_and_convert(url)
-    await ctx.send(f'Archivo {name_file_pdf} creado.', file=discord.File(name_file_pdf))
+    try:
+        await ctx.send("Downloading and converting the file... This may take some time.")
 
+        name_file_pdf = await asyncio.to_thread(download_and_convert, url)
+
+        await asyncio.sleep(1)
+
+        await ctx.send(f'File {name_file_pdf} created.', file=discord.File(name_file_pdf))
+    except Exception as e:
+        await ctx.send("An error occurred during file conversion.")
+        print(f"Error during conversion: {str(e)}")
+        
+@bot.command('doc')
+async def convert(ctx, url: str):
+    try:
+        await ctx.send("Downloading and converting the file... This may take some time.")
+
+        name_file_docx = await asyncio.to_thread(convert_pdf_to_word, url)
+
+        await asyncio.sleep(1)
+
+        await ctx.send(f'File {name_file_docx} created.', file=discord.File(name_file_docx))
+    except Exception as e:
+        await ctx.send("An error occurred during conversion to Word format.")
+        print(f"Error during conversion: {str(e)}")
 bot.run(TOKEN)
